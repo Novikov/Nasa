3df312b1799761f7ab6bd1a3076b0b7275bbb5d3
Igor Novikov <novikov.mail@inbox.ru>
2021-02-10 09:34:59 +0300

    app navigation works

--- app/src/main/java/com/nasa/app/media/MediaPreviewFragment.kt
+++ app/src/main/java/com/nasa/app/media/MediaPreviewFragment.kt
@@ -5,29 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.nasa.app.R
+import kotlinx.android.synthetic.main.fragment_media.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MediaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MediaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+
+
+
     }
 
     override fun onCreateView(
@@ -35,26 +28,17 @@ class MediaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_media, container, false)
+
+        val view =  inflater.inflate(R.layout.fragment_media, container, false)
+
+        val button = view.findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_mediaFragment_to_imageFragmentDetails)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MediaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MediaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
