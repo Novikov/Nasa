150af18dd403b794cb20a7ac429e837871605af6
Igor Novikov <novikov.mail@inbox.ru>
2021-02-14 11:35:10 +0300

    DetailMediaFragment edits

--- app/src/main/java/com/nasa/app/media_detail/DetailMediaFragment.kt
+++ app/src/main/java/com/nasa/app/media_detail/DetailMediaFragment.kt
@@ -1,19 +1,16 @@
 package com.nasa.app.media_detail
 
-import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ui.PlayerControlView
 import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.ui.StyledPlayerControlView
 import com.nasa.app.R
 import com.squareup.picasso.Picasso
 
@@ -27,11 +24,22 @@ class DetailMediaFragment : Fragment() {
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_media_detail, container, false)
 
+//Spinner
+        val spinner: Spinner = view.findViewById(R.id.download_button)
+        val adapter = ArrayAdapter(
+            requireContext(), R.layout.custom_spinner, resources.getStringArray(R.array.list)
+        )
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown);
+        spinner.adapter = adapter;
+
+
+
+
 //        val playerView = view.findViewById<PlayerView>(R.id.exo_player_video_view)
 //
 //        playerView.visibility = View.GONE
 //
-//        val img = view.findViewById<ImageView>(R.id.image_media)
+//        val img = view.findViewById<ImageView>(R.id.image_media_view)
 //
 //        Picasso
 //            .get()
@@ -45,11 +53,13 @@ class DetailMediaFragment : Fragment() {
 
         val player = SimpleExoPlayer.Builder(requireContext()).build()
 
-        val mediaItem: MediaItem = MediaItem.fromUri("https://images-assets.nasa.gov/video/0091%20NEA%20Scout%20Music%20Rev29%2002102021/0091%20NEA%20Scout%20Music%20Rev29%2002102021~orig.mp4")
+        val mediaItem: MediaItem = MediaItem.fromUri("https://images-assets.nasa.gov/video/Expedition_58_Museum_Red_Square_Visit_with_Interpreter_2018_1115_1545_724102/Expedition_58_Museum_Red_Square_Visit_with_Interpreter_2018_1115_1545_724102~orig.mp4")
         player.setMediaItem(mediaItem)
         playerView.player = player
         player.prepare()
         player.play()
+
+
 
         return view
     }
