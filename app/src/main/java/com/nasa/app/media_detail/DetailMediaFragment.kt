9d8d5592e4a897ff3ab053118f22ad11ae398597
Igor Novikov <novikov.mail@inbox.ru>
2021-02-14 17:32:06 +0300

    DetailMediaFragment layout edits

--- app/src/main/java/com/nasa/app/media_detail/DetailMediaFragment.kt
+++ app/src/main/java/com/nasa/app/media_detail/DetailMediaFragment.kt
@@ -25,39 +25,40 @@ class DetailMediaFragment : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_media_detail, container, false)
 
 //Spinner
-        val spinner: Spinner = view.findViewById(R.id.download_button)
+        val spinner: Spinner = view.findViewById(R.id.download_spinner)
         val adapter = ArrayAdapter(
             requireContext(), R.layout.custom_spinner, resources.getStringArray(R.array.list)
         )
+
         adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown);
         spinner.adapter = adapter;
 
 
 
 
-//        val playerView = view.findViewById<PlayerView>(R.id.exo_player_video_view)
-//
-//        playerView.visibility = View.GONE
-//
-//        val img = view.findViewById<ImageView>(R.id.image_media_view)
-//
-//        Picasso
-//            .get()
-//            .load("https://images-assets.nasa.gov/image/MSFC-202100007/MSFC-202100007~medium.jpg")
-//            .into(img);
-//
-//        img.adjustViewBounds = true
-
-
         val playerView = view.findViewById<PlayerView>(R.id.exo_player_video_view)
 
-        val player = SimpleExoPlayer.Builder(requireContext()).build()
+        playerView.visibility = View.GONE
 
-        val mediaItem: MediaItem = MediaItem.fromUri("https://images-assets.nasa.gov/video/Expedition_58_Museum_Red_Square_Visit_with_Interpreter_2018_1115_1545_724102/Expedition_58_Museum_Red_Square_Visit_with_Interpreter_2018_1115_1545_724102~orig.mp4")
-        player.setMediaItem(mediaItem)
-        playerView.player = player
-        player.prepare()
-        player.play()
+        val img = view.findViewById<ImageView>(R.id.image_media_view)
+
+        Picasso
+            .get()
+            .load("https://images-assets.nasa.gov/image/MSFC-202100007/MSFC-202100007~medium.jpg")
+            .into(img);
+
+        img.adjustViewBounds = true
+
+
+//        val playerView = view.findViewById<PlayerView>(R.id.exo_player_video_view)
+//
+//        val player = SimpleExoPlayer.Builder(requireContext()).build()
+//
+//        val mediaItem: MediaItem = MediaItem.fromUri("https://images-assets.nasa.gov/video/Expedition_58_Museum_Red_Square_Visit_with_Interpreter_2018_1115_1545_724102/Expedition_58_Museum_Red_Square_Visit_with_Interpreter_2018_1115_1545_724102~orig.mp4")
+//        player.setMediaItem(mediaItem)
+//        playerView.player = player
+//        player.prepare()
+//        player.play()
 
 
 
