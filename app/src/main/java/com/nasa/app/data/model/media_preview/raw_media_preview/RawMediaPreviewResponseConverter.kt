5380b342c59fb20416eec9090dffd45c317ed2e5
Igor Novikov <novikov.mail@inbox.ru>
2021-05-21 21:33:23 +0300

    RawMediaPreviewResponseConverter test

--- app/src/main/java/com/nasa/app/data/model/media_preview/raw_media_preview/RawMediaPreviewResponseConverter.kt
+++ app/src/main/java/com/nasa/app/data/model/media_preview/raw_media_preview/RawMediaPreviewResponseConverter.kt
@@ -1,10 +1,12 @@
 package com.nasa.app.data.model.media_preview.raw_media_preview
 
+import android.util.Log
 import com.nasa.app.data.model.ContentType
 import com.nasa.app.data.model.media_preview.MediaPreview
 import com.nasa.app.data.model.media_preview.MediaPreviewResponse
 import com.nasa.app.utils.EMPTY_STRING
 import com.nasa.app.utils.POST_PER_PAGE
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
@@ -81,26 +83,26 @@ class RawMediaPreviewResponseConverter @Inject constructor() {
     }
 
     private fun convertDate(dateStr: String): String {
-        val parsedDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
-        val parsedDate: Date? = parsedDateFormat.parse(dateStr)
+        val parsedDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val targetFormat = SimpleDateFormat("MMM d, yyyy")
+        var parsedDate: Date? = null
 
-        if (parsedDate != null) {
-
-            val dayDateFormat = SimpleDateFormat("dd")
-            val day = dayDateFormat.format(parsedDate)
-
-            val monthDateFormat = SimpleDateFormat("MMMM")
-            val month = monthDateFormat.format(parsedDate)
-
-            val yearDateFormat = SimpleDateFormat("yyyy")
-            val year = yearDateFormat.format(parsedDate)
-
-            val dateCreated = "$month $day, $year"
-
-            return dateCreated
-
-        } else {
-            return EMPTY_STRING
+        try {
+            parsedDate = parsedDateFormat.parse(dateStr)
         }
+        catch (ex:ParseException){
+            Log.i(TAG, "convertDate: parsing error")
+        }
+
+        return if (parsedDate != null) {
+            val dateCreated = targetFormat.format(parsedDate)
+            dateCreated.toString()
+        } else {
+            EMPTY_STRING
+        }
+    }
+
+    companion object {
+        private const val TAG = "RawMediaPreviewResponse"
     }
 }
--- app/src/test/java/com/nasa/app/data/model/media_preview/raw_media_preview/RawMediaPreviewResponseConverterTest.kt
+++ app/src/test/java/com/nasa/app/data/model/media_preview/raw_media_preview/RawMediaPreviewResponseConverterTest.kt
@@ -2,16 +2,76 @@ package com.nasa.app.data.model.media_preview.raw_media_preview
 
 import com.google.common.truth.Truth.assertThat
 import com.google.gson.Gson
+import com.nasa.app.data.model.ContentType
 import org.junit.Test
 import java.io.FileReader
 
 class RawMediaPreviewResponseConverterTest {
-    val jsonPath = "/home/x/GIT/Nasa/app/src/test/java/com/nasa/app/data/model/media_preview/raw_media_preview/RawMediaPreviewResponse.json"
-    val gson = Gson()
+    val longJsonResponsePath = "src/test/resources/media_preview/LongMediaPreviewResponse.json"
+    val shortJsonResponsePath = "src/test/resources/media_preview/ShortMediaPreviewResponse.json"
+    val singleImageJsonResponsePath = "src/test/resources/media_preview/SingleImageMediaPreviewResponse.json"
+    val singleVideoJsonResponsePath = "src/test/resources/media_preview/SingleVideoMediaPreviewResponse.json"
+    val singleAudioJsonResponsePath = "src/test/resources/media_preview/SingleAudioMediaPreviewResponse.json"
+    val mediaPreviewResponseConverter = RawMediaPreviewResponseConverter()
+
+    /**
+     * 1.Converter should correct count totalPages
+     * 2.Converter should cut description
+     * 3.Converter should convert date?
+     * 4.Converter should convert data type
+     * */
 
     @Test
-    fun `media previews response converter should convert images`(){
-        val rawMediaPreviewResponse = gson.fromJson(FileReader(jsonPath),RawMediaPreviewResponse::class.java)
-        assertThat(rawMediaPreviewResponse.collection.items.size).isEqualTo(1)
+    fun `total pages property should be more 1 for total_hits more than 100`(){
+        val rawMediaPreviewResponse = getResponseFromJson(longJsonResponsePath)
+        val convertedResponse = mediaPreviewResponseConverter.getMediaPreviewResponse(rawMediaPreviewResponse)
+        assertThat(convertedResponse.totalPages).isEqualTo(59)
+    }
+
+    @Test
+    fun `total pages property should equals 1 for total_hits less than 100 `(){
+        val rawMediaPreviewResponse = getResponseFromJson(shortJsonResponsePath)
+        val convertedResponse = mediaPreviewResponseConverter.getMediaPreviewResponse(rawMediaPreviewResponse)
+        assertThat(convertedResponse.totalPages).isEqualTo(1)
+    }
+
+    @Test
+    fun `description property should be shortened to 200 characters if it greater`(){
+        val rawMediaPreviewResponse = getResponseFromJson(singleImageJsonResponsePath)
+        val convertedResponse = mediaPreviewResponseConverter.getMediaPreviewResponse(rawMediaPreviewResponse)
+        assertThat(convertedResponse.mediaPreviewList[0].description.length).isEqualTo(200)
+    }
+
+    @Test
+    fun `date property have to match the format (MMM d, yyyy)`(){
+        val rawMediaPreviewResponse = getResponseFromJson(singleImageJsonResponsePath)
+        val convertedResponse = mediaPreviewResponseConverter.getMediaPreviewResponse(rawMediaPreviewResponse)
+        assertThat(convertedResponse.mediaPreviewList[0].dateCreated).isEqualTo("Apr 1, 2011")
+    }
+
+    @Test
+    fun `images items should have ContentType enum type`(){
+        val rawMediaPreviewResponse = getResponseFromJson(singleImageJsonResponsePath)
+        val convertedResponse = mediaPreviewResponseConverter.getMediaPreviewResponse(rawMediaPreviewResponse)
+        assertThat(convertedResponse.mediaPreviewList[0].mediaType).isEqualTo(ContentType.IMAGE)
+    }
+
+    @Test
+    fun `video items should have ContentType enum type`(){
+        val rawMediaPreviewResponse = getResponseFromJson(singleVideoJsonResponsePath)
+        val convertedResponse = mediaPreviewResponseConverter.getMediaPreviewResponse(rawMediaPreviewResponse)
+        assertThat(convertedResponse.mediaPreviewList[0].mediaType).isEqualTo(ContentType.VIDEO)
+    }
+
+    @Test
+    fun `audio items should have ContentType enum type`(){
+        val rawMediaPreviewResponse = getResponseFromJson(singleAudioJsonResponsePath)
+        val convertedResponse = mediaPreviewResponseConverter.getMediaPreviewResponse(rawMediaPreviewResponse)
+        assertThat(convertedResponse.mediaPreviewList[0].mediaType).isEqualTo(ContentType.AUDIO)
+    }
+
+    private fun getResponseFromJson(path:String): RawMediaPreviewResponse {
+        val gson = Gson()
+        return gson.fromJson(FileReader(path),RawMediaPreviewResponse::class.java)
     }
 }
